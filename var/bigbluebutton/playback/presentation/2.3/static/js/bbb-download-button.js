1964db7e8dab13565909b2af51bf0bf4a5dd6d69
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-13 02:15:01 +0800

    Update bbb-download-button.js

--- var/bigbluebutton/playback/presentation/2.3/static/js/bbb-download-button.js
+++ var/bigbluebutton/playback/presentation/2.3/static/js/bbb-download-button.js
@@ -35,26 +35,28 @@ const $el = await _waitForElement(".top-bar");
   download_button_el.className += " circle";
   download_button_el_wrapper.appendChild(download_button_el);
   
-  // Set a download image icon
-  download_button_el_img = document.createElement('img');
-  download_button_el_img.src = "/playback/presentation/2.3/static/media/download.png"
-
-
   var check_url="/presentation/"+document.URL.split("/")[6]+"/video.mp4";
   var http = new XMLHttpRequest();
   http.open('HEAD', check_url, false);
   http.send();
   if (http.status != 404) {
-      download_button_el.setAttribute("href", check_url);
-
-      // Rename video.mp4 to <ID>.mp4 when downloading
-      download_button_el.setAttribute("download", document.URL.split("/")[6] + ".mp4");
+    download_button_el_img_download = document.createElement('img');
+    download_button_el_img_download.src = "/playback/presentation/2.3/static/media/download.png"
+    
+    download_button_el.appendChild(download_button_el_img_download);
+    download_button_el.setAttribute("href", check_url);
+    
+    // Rename video.mp4 to <ID>.mp4 when downloading
+    download_button_el.setAttribute("download", document.URL.split("/")[6] + ".mp4");    
   } else {
-      download_button_el_icon.setAttribute("class","icon-close");
-      download_button_el.setAttribute("title", "MP4 file is not ready yet! Reload page later!");
-      download_button_el.setAttribute("onclick", "alert('MP4 file is not ready yet! Reload page later!');");
+    download_button_el_img_wait = document.createElement('img');
+    download_button_el_img_wait.src = "/playback/presentation/2.3/static/media/wait.png"
+    download_button_el_img_wait.setAttribute("title", "MP4 file is not ready yet! Reload page later!");
+    download_button_el_img_wait.setAttribute("onclick", "alert('MP4 file is not ready yet! Reload page later!');");
+    
+    download_button_el.appendChild(download_button_el_img_download);
   }
-  download_button_el.appendChild(download_button_el_img);
+  
   document.getElementsByClassName('left')[0].appendChild(download_button_el_wrapper);
 
 })();
