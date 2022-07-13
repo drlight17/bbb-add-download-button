203b08ba63d4a5428128e90cc524c0927e0e8fdb
drlight17 <37434652+drlight17@users.noreply.github.com>
2022-07-13 11:44:17 +0300

    some fixes and new stuff
    new function for file check
    new titles and lables
    file size in download link title and lable

--- var/bigbluebutton/playback/presentation/2.3/static/js/bbb-download-button.js
+++ var/bigbluebutton/playback/presentation/2.3/static/js/bbb-download-button.js
@@ -1,62 +1,73 @@
-var file_content;
+var file_size;
+var file_status;
 var check_url="/presentation/"+document.URL.split("/")[6]+"/video.mp4";
 
-Promise.all([
-  fetch(check_url).then(x => x.text())
-]).then((sampleResp) => {
-  file_content = sampleResp;
-});
+get_filesize(check_url);
+
+function get_filesize(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("HEAD", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            file_size = parseInt(xhr.getResponseHeader("Content-Length"));
+            file_status = xhr.status;
+        }
+    };
+    xhr.send();
+}
 
 function _waitForElement(selector, delay = 50, tries = 10000) {
-    const element = document.querySelector(selector);
-    if (!window[`__${selector}`]) {
+  const element = document.querySelector(selector);
+  if (!window[`__${selector}`]) {
+    window[`__${selector}`] = 0;
+  }
+  function _search() {
+    return new Promise((resolve) => {
+      window[`__${selector}`]++;
+      setTimeout(resolve, delay);
+    });
+  }
+  if (element === null) {
+    if (window[`__${selector}`] >= tries) {
       window[`__${selector}`] = 0;
+      return Promise.reject(null);
     }
-    function _search() {
-      return new Promise((resolve) => {
-        window[`__${selector}`]++;
-        setTimeout(resolve, delay);
-      });
-    }
-    if (element === null) {
-      if (window[`__${selector}`] >= tries) {
-        window[`__${selector}`] = 0;
-        return Promise.reject(null);
-      }
-      return _search().then(() => _waitForElement(selector));
-    } else {
-      return Promise.resolve(element);
-    }
+    return _search().then(() => _waitForElement(selector));
+  } else {
+    return Promise.resolve(element);
+  }
 };
 
-const start = (async () => {
-    const $el = await _waitForElement(".top-bar");
+const start = async () => {
+const $el = await _waitForElement(".top-bar");
+  download_button_el_wrapper = document.createElement('div');
+  download_button_el_wrapper.setAttribute("class", "button-wrapper");
+  download_button_el = document.createElement('a');
+  download_button_el.setAttribute("aria-label", "Presentation download is not ready yet! Please, reload page later!");
+  download_button_el.setAttribute("title", "Presentation download is not ready yet! Please, reload page later!");
+  download_button_el.setAttribute("onclick", "alert('Presentation download is not ready yet! Please, reload page later!');");
+  download_button_el.style.cssText = 'text-decoration: none;';
+  download_button_el.className += "button";
+  download_button_el.className += " default";
+  download_button_el.className += " circle";
+  download_button_el_wrapper.appendChild(download_button_el);
+  download_button_el_icon = document.createElement('span');
+  download_button_el_icon.className += "icon-close";
 
-    download_button_el_wrapper = document.createElement('div');
-    download_button_el_wrapper.setAttribute("class", "button-wrapper");
-    download_button_el = document.createElement('a');
-    download_button_el.setAttribute("aria-label", "Download MP4 presentation");
-    download_button_el.setAttribute("title", "Download MP4 presentation");
-    download_button_el.setAttribute("download","video.mp4");
-    download_button_el.style.cssText = 'text-decoration: none;';
-    download_button_el.className += "button";
-    download_button_el.className += " default";
-    download_button_el.className += " circle";
-    download_button_el_wrapper.appendChild(download_button_el);
-    download_button_el_icon = document.createElement('span');
-    download_button_el_icon.className += "icon-videos";
+  //console.log(file_size);
+  //console.log(file_status);
 
-    var http = new XMLHttpRequest();
-    http.open('HEAD', check_url, false);
-    http.send();
-    //console.log(file_content);
-    if ((http.status != 404)&&(file_content[0].indexOf("PLACEHOLDER") < 0)) {
+      if ((file_status != 404)&&(file_size != 103)&&(file_size !='NaN')) {
+        download_button_el.innerHTML = '';
+        download_button_el.removeAttribute("onclick");
+        download_button_el_icon.setAttribute("class","icon-videos");
         download_button_el.setAttribute("href", check_url);
-    } else {
-        download_button_el_icon.setAttribute("class","icon-close");
-        download_button_el.setAttribute("title", "MP4 file is not ready yet! Reload page later!");
-        download_button_el.setAttribute("onclick", "alert('MP4 file is not ready yet! Reload page later!');");
-    }
-    download_button_el.appendChild(download_button_el_icon);
-    document.getElementsByClassName('left')[0].appendChild(download_button_el_wrapper);
-})();
+        download_button_el.setAttribute("aria-label", "Download presentation. File size is "+Math.trunc(file_size/1024/1024)+" MB");
+        download_button_el.setAttribute("title", "Download presentation. File size is "+Math.trunc(file_size/1024/1024)+" MB");
+      }
+  //}
+  download_button_el.appendChild(download_button_el_icon);
+  document.getElementsByClassName('left')[0].appendChild(download_button_el_wrapper);
+};
+
+start();
